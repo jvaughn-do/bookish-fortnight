91c7b3125dda2144883747b7ef885981e51b5187
jvaughn-do <133175092+jvaughn-do@users.noreply.github.com>
2024-08-07 13:41:27 -0700

    Update worker protocol

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 onmessage = (e) => {
   console.log("Message received from main script");
   var result
-  fetch("0.0.0.0:8080/info")
+  fetch("http://0.0.0.0:8080/info")
   .then((response) => {
     console.log(response.body);
     result = response.body
